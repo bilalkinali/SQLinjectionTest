e9caf74f7efa047dcf3a33929a3a80b242afb9b2
Bilal Kinali <biki59986@edu.ucl.dk>
2024-02-23 11:22:36 +0100

    Add project files.

---
+++ SQLinjectionTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Data.SqlClient;
+
+string connectionString = "Server=BILAL-KINALI;Database=UserDb;Trusted_Connection=True;";
+
+Console.Write("Brugernavn: ");
+string username = Console.ReadLine();
+
+Console.Write("Password: ");
+string password = Console.ReadLine();
+
+using (SqlConnection conn = new SqlConnection(connectionString))
+{
+    string query = $"SELECT COUNT(*) From Users WHERE Username = @username AND Password = @password";
+
+    using (SqlCommand command = new SqlCommand(query, conn))
+    {
+        command.Parameters.AddWithValue("@username", username);
+        command.Parameters.AddWithValue("@password", password);
+
+        try
+        {
+            conn.Open();
+            int userCount = (int)command.ExecuteScalar();
+
+            if (userCount > 0)
+            {
+                Console.WriteLine("Match");
+            }
+            else
+            {
+                Console.WriteLine("No match");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("En fejl opstod " + ex.Message);
+        }
+        finally
+        {
+            conn.Close();
+        }
+    }
+}
